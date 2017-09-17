239dbb96d19e35d01dbb0547a0f6df4703af8541
Micah Buckley-Farlee <micahbuckleyfarlee@gmail.com>
2017-09-17 12:07:41 -0700

    Use cancelAndHoldAtTime to mitigate clicking

--- src/goom/synth/envelope.cljs
+++ src/goom/synth/envelope.cljs
@@ -7,6 +7,6 @@
     (let [param (aget node (clj->js param-name))
           ctx   ($ node -context)
           now   ($ ctx -currentTime)]
-      ($ param cancelScheduledValues now)
+      ($ param cancelAndHoldAtTime now)
       ($ param exponentialRampToValueAtTime 1 (+ now attack))
       ($ param exponentialRampToValueAtTime 0.0000001 (+ now attack decay)))))
