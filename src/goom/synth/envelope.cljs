e397de902011c4013fc06916213aa6497fa5c281
Micah Buckley-Farlee <micahbuckleyfarlee@gmail.com>
2017-09-17 17:20:58 -0700

    Recalculate now

--- src/goom/synth/envelope.cljs
+++ src/goom/synth/envelope.cljs
@@ -6,7 +6,7 @@
   (fn [node param-name]
     (let [param (aget node (clj->js param-name))
           ctx   ($ node -context)
-          now   ($ ctx -currentTime)]
-      ($ param cancelAndHoldAtTime now)
-      ($ param exponentialRampToValueAtTime 1 (+ now attack))
-      ($ param exponentialRampToValueAtTime 0.0000001 (+ now attack decay)))))
+          now   #($ ctx -currentTime)]
+      ($ param cancelAndHoldAtTime (now))
+      ($ param exponentialRampToValueAtTime 1 (+ (now) attack))
+      ($ param exponentialRampToValueAtTime 0.0000001 (+ (now) attack decay)))))
